ee3d36aeccc9f4504348093aaf4e08770afb9262
ayfererdogan <104556757+ayfererdogan@users.noreply.github.com>
2022-05-13 23:12:49 +0300

    New line
    s

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -1,7 +1,7 @@
 import { storage, Context } from "near-sdk-as"
 
 export function helloWorld(names: Array<string>): string {
-  return names.map<string>(name => 'hello ' + name).join('\n')
+  return names.map<string>(name => 'hello ' + name).join(`\n`)
 }
 
 // read the given key from account (contract) storage
